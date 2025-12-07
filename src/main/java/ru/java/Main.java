9f3589fac292d5759c4a41b971ddf3d1adfeb8ea
Olesya Bystrushkina <testermpinvest@gmail.com>
2025-12-07 19:34:59 +0300

    test

--- src/main/java/ru/java/Main.java
+++ src/main/java/ru/java/Main.java
@@ -28,6 +28,6 @@ public class Main {
         }
         System.out.println("Наш массив" + Arrays.toString(numbers));
         double result = Sum.summator(numbers);
-        System.out.println("Результат: " + result);
+        System.out.println("РезультатGHBDDFGH: " + result);
     }
 }
