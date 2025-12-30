b56fe45885f84b927c05e17d9b0a90b61de0affa
Olesya Bystrushkina <testermpinvest@gmail.com>
2025-12-30 13:48:58 +0300

    Повторное исправление задания "Практика ООП. Исключения #1. NumberFormatException"

--- src/main/java/ru/java/Main.java
+++ src/main/java/ru/java/Main.java
@@ -1,10 +1,8 @@
 package ru.java;
 
+import ru.java.exception.Sum;
 import ru.java.sauce.ESpices;
 import ru.java.sauce.Sauce;
-
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,32 +16,6 @@ public class Main {
 
         System.out.println("-------------");
         System.out.println("Практика ООП. Исключения #1. NumberFormatException");
-        Scanner sc = new Scanner(System.in);// Создаем сканер
-        System.out.println("Сколько чисел вы хотите ввести: ");
-        int count = Integer.parseInt(sc.nextLine());
-        System.out.println("Введите числа");
-
-        String[] numbers = new String[count];
-        for (int i = 0; i < count; i++) {
-            numbers[i] = sc.nextLine();
-        }
-        System.out.println("Наш массив" + Arrays.toString(args));
-        Sum.main(numbers);
-    }
-}
-
-class Sum {
-
-    public static void main(String[] args) {
-        double sum = 0;
-        for (int i = 0; i < args.length; i++) {
-            try {
-                double number = Double.parseDouble(args[i]);
-                sum += number;
-            } catch (NumberFormatException e) {
-                System.out.println(args[i] + "- не является числом");
-            }
-        }
-        System.out.println("Результат= " + sum);
+        Sum.main(args);
     }
 }
